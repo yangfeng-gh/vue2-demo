1a80b7e803a8547636e0ca406d5ff679493565cf
yvan <57882634@qq.com>
2018-12-09 15:53:52 +0800

    app.vue add storage

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,31 @@
 </template>
 
 <script>
+import { APPNAME } from '@/lib/consts'
+import { debounce } from '@/lib/event'
+
 export default {
-  name: 'app'
+  name: 'App',
+  created () {
+    // 在页面加载时读取sessionStorage里的状态信息
+    if (localStorage.getItem(APPNAME)) {
+      this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(localStorage.getItem(APPNAME))))
+    }
+
+    // 在页面刷新时将vuex里的信息保存到sessionStorage里
+    window.addEventListener('beforeunload', () => {
+      localStorage.setItem(APPNAME, JSON.stringify(this.$store.state))
+    })
+  },
+  mounted () {
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        this.$store.commit('app/setMainMenuShrink', document.documentElement.clientWidth < 1230)
+        this.$store.commit('app/setLeftMenuShrink', document.documentElement.clientWidth < 1230)
+      }, 300)
+    )
+  }
 }
 </script>
 
---
+++ src/lib/consts.js
@@ -0,0 +1,1 @@
+export const APPNAME = 'VUE2-DEMO'
